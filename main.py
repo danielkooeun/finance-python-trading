3e2f2aef8ac9fa6a434cdc86f58895ef0434db2b
Daniel Koo <fhisa2@gmail.com>
2018-08-17 02:07:58 -0400

    more daily return analysis

--- main.py
+++ main.py
@@ -3,34 +3,22 @@ import pandas as pd
 pd.core.common.is_list_like = pd.api.types.is_list_like
 
 import pandas_datareader as pdr
-import quandl
 import datetime
+import quandl
+quandl.ApiConfig.api_key = '-nT3YEYhZVvDaJ5Bx9pE'
 
-# aapl = quandl.get('WIKI/AAPL', start_date='2006-10-01', end_date='2018-01-01')
-# aapl.to_csv('data/aapl_ohlc.csv')
+aapl = quandl.get('WIKI/AAPL', start_date='2006-10-01', end_date='2018-01-01')
+aapl.to_csv('data/aapl_ohlc.csv')
 
 # df will contain dataframe for AAPL time series data
 aapl = pd.read_csv('data/aapl_ohlc.csv', header=0, index_col='Date', parse_dates=True)
 
-# indices contain the date values, columns show the data for each date
-aapl_index = aapl.index
-aapl_columns = aapl.columns
+def get(tickers, startdate, enddate):
+  def data(ticker):
+    return (quandl.get('WIKI/' + ticker, start_date=startdate, end_date=enddate))
+  datas = map (data, tickers)
+  return(pd.concat(datas, keys=tickers, names=['Ticker', 'Date']))
 
-# returns Series
-# print(aapl_columns[-10:])
-# print(aapl.loc[pd.Timestamp('2006-11-01'):pd.Timestamp('2006-12-31')].head())
-# print(aapl.loc['2007'].head())
-# print(aapl.iloc[[22, 43], [0, 3]])
-
-# Samples 20 rows (random)
-# sample = aapl.sample(20)
-# print(sample)
-
-# Change sample to months
-# print(aapl.resample('M').mean())
-# print(aapl.asfreq('M', method='bfill'))
-
-# Add column 'diff' to 'aapl'
-# aapl['diff'] = aapl.Open - aapl.Close
-# print(aapl['diff'].resample('M').mean())
-# del aapl['diff']
+tickers = ['AAPL', 'MSFT', 'IBM', 'GOOG']
+all_data = get(tickers, '2006-10-01', '2018-01-01')
+all_data.to_csv('data/faang_stock.csv')
